9d50e78432d060cda0fff627fd8a70c6b69f292f
Witixin1512 <73248264+Witixin1512@users.noreply.github.com>
2022-10-05 13:35:26 +0200

    Add fabric recipetype and config

--- Fabric/src/main/java/com/hrznstudio/emojiful/EmojifulFabric.java
+++ Fabric/src/main/java/com/hrznstudio/emojiful/EmojifulFabric.java
@@ -1,10 +1,20 @@
 package com.hrznstudio.emojiful;
 
+import com.hrznstudio.emojiful.datapack.EmojiRecipe;
+import com.hrznstudio.emojiful.datapack.EmojiRecipeSerializer;
+import com.hrznstudio.emojiful.platform.FabricConfigHelper;
+import eu.midnightdust.lib.config.MidnightConfig;
 import net.fabricmc.api.ModInitializer;
+import net.minecraft.world.item.crafting.RecipeSerializer;
+import net.minecraft.world.item.crafting.RecipeType;
 
 public class EmojifulFabric implements ModInitializer {
+
+
+    public static final RecipeType<EmojiRecipe> EMOJI_RECIPE_TYPE = RecipeType.register(Constants.MOD_ID + "emoji_recipe_type");
+    public static final RecipeSerializer<EmojiRecipe> EMOJI_RECIPE_SERIALIZER = RecipeSerializer.register(Constants.MOD_ID + "emoji_recipe", new EmojiRecipeSerializer());
     @Override
     public void onInitialize() {
-        //Register config, serializer and recipetype, handle recipetype reloading too
+        MidnightConfig.init(Constants.MOD_ID, FabricConfigHelper.class);
     }
 }
