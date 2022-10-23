4d8652a961a24252047ef6e30649b8cbe1b33cd8
Witixin1512 <73248264+Witixin1512@users.noreply.github.com>
2022-10-23 15:39:38 +0200

    Revert "Migrate to a static final constant, make sure the bed can autocomplete"
    This reverts commit f5b3c3b262a209a6712c1e9cf03d8a4da1e807a2.

--- Common/src/main/java/com/hrznstudio/emojiful/gui/EmojifulBedChatScreen.java
+++ Common/src/main/java/com/hrznstudio/emojiful/gui/EmojifulBedChatScreen.java
@@ -5,7 +5,6 @@ import com.hrznstudio.emojiful.Constants;
 import com.hrznstudio.emojiful.platform.Services;
 import com.mojang.blaze3d.vertex.PoseStack;
 import net.minecraft.client.gui.screens.InBedChatScreen;
-import org.lwjgl.glfw.GLFW;
 
 public class EmojifulBedChatScreen extends InBedChatScreen {
 
@@ -35,7 +34,7 @@ public class EmojifulBedChatScreen extends InBedChatScreen {
 
     @Override
     public boolean keyPressed(int keyCode, int scanCode, int modifiers) {
-        if (super.keyPressed(keyCode, scanCode, modifiers) && keyCode != GLFW.GLFW_KEY_TAB){
+        if (super.keyPressed(keyCode, scanCode, modifiers)){
             return true;
         }
         if (emojiSuggestionHelper != null && emojiSuggestionHelper.keyPressed(keyCode, scanCode, modifiers))
